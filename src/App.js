034a8084a63d559c9e9605c10b98478187f313c1
Musak2 <63206033+Musak2@users.noreply.github.com>
2023-10-31 20:28:27 +0100

    New Category, New photo, Popups
    Added box for adding new category and box for adding new photos with styles. Added popups for entering category and photo information.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Box from './components/Box';
+import TextField from '@mui/material/TextField';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+
+
 
 function App() {
   const [boxes, setBoxes] = useState([]);
@@ -9,6 +13,7 @@ function App() {
   const maxBoxesInRow = 4;
   const [previewImage, setPreviewImage] = useState(null);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
 
@@ -112,6 +117,14 @@ function App() {
     }
   };
 
+  const handleBoxClick = () => {
+    setShowPopup(true);  // Show the popup when the box is clicked
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false); // Hide the popup
+  };
+
   return (
     <div className="App">
       <div className="fotogaleria-text">Fotogaléria</div>
@@ -121,30 +134,119 @@ function App() {
       </div>
 
       {/* If not viewing a specific category, show all categories */}
-      {!viewingCategory && boxes.map((box, index) => {
-        return (
-          <Box
-            key={box.id}
-            top={225 + Math.floor(index / maxBoxesInRow) * (295 + 32)}
-            left={304 + (index % maxBoxesInRow) * (304 + 32)}
-            text={box.text}
-            mainImage={box.mainImage}
-            onClick={() => {handleCategoryClick(box);}}
-            imageCount={box.numberOfImages}
-          />
-        );
-      })}
+      {!viewingCategory && (
+        <>
+          {boxes.map((box, index) => {
+            return (
+              <Box
+                key={box.id}
+                top={225 + Math.floor(index / maxBoxesInRow) * (295 + 32)}
+                left={304 + (index % maxBoxesInRow) * (304 + 32)}
+                text={box.text}
+                mainImage={box.mainImage}
+                onClick={() => { handleCategoryClick(box); }}
+                imageCount={box.numberOfImages}
+              />
+            );
+          })}
+
+          <div
+            className="box"
+            style={{
+              top: `${225 + Math.floor(boxes.length / maxBoxesInRow) * (295 + 32)}px`,
+              left: `${304 + (boxes.length % maxBoxesInRow) * (304 + 32)}px`,
+              cursor: 'pointer',
+              display: 'flex',  // Added this line
+              flexDirection: 'column',
+              justifyContent: 'center', // Added this line
+              alignItems: 'center',
+            }}
+            onClick={handleBoxClick}
+          >
+            <div className="plus-symbol" style={{ display: 'flex', fontSize: '24px', justifyContent: 'center', alignItems: 'center' }}>
+            </div>
+            <div className="addText" style={{ height: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              Pridať kategóriu
+            </div>
+          </div>
+
+          {showPopup && (
+            <div className="preview-overlay" onClick={handleClosePopup}>
+              <div className="popup-box" onClick={e => e.stopPropagation()} style={{ paddingLeft: '30px', paddingRight: '30px', paddingTop: '30px' }}>
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '30px' }}>
+                  <h3>Pridať kategóriu</h3>
+                  <span style={{ cursor: 'pointer', fontSize: '30px', }} onClick={handleClosePopup}>x</span>
+                </div>
+                <TextField
+                  label="Názov kategórie"
+                  variant="outlined"
+                  fullWidth
+                  className='textfield'
+                />
+                <div style={{ paddingTop: '40px' }}>
+                  <button type="submit" style={{ height: '60px' }}>Pridať</button>
+                </div>
+              </div>
+            </div>
+          )}
+        </>
+      )}
 
       {/* If viewing a specific category, show its images */}
-      {viewingCategory && viewingCategory.relatedImages.map((image, index) => (
-        <Box
-          key={index}
-          top={225 + Math.floor(index / maxBoxesInRow) * (295 + 32)}
-          left={304 + (index % maxBoxesInRow) * (304 + 32)}
-          mainImage={image}
-          onClick={() => openPreview(viewingCategory.text, image.split('/').pop(), index)}
-        />
-      ))}
+      {
+        viewingCategory && (
+          <>
+            {viewingCategory.relatedImages.map((image, index) => (
+              <Box
+                key={index}
+                top={225 + Math.floor(index / maxBoxesInRow) * (295 + 32)}
+                left={304 + (index % maxBoxesInRow) * (304 + 32)}
+                mainImage={image}
+                onClick={() => openPreview(viewingCategory.text, image.split('/').pop(), index)}
+              />
+            ))}
+
+            <div
+              className="box"
+              style={{
+                top: `${225 + Math.floor(viewingCategory.relatedImages.length / maxBoxesInRow) * (295 + 32)}px`,
+                left: `${304 + (viewingCategory.relatedImages.length % maxBoxesInRow) * (304 + 32)}px`,
+                cursor: 'pointer',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              onClick={handleBoxClick}
+            >
+              <div className="plus-symbol" style={{ display: 'flex', fontSize: '24px', justifyContent: 'center', alignItems: 'center' }}>
+              </div>
+              <div className="addText" style={{ height: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                Pridať fotky
+              </div>
+            </div>
+
+            {showPopup && (
+              <div className="preview-overlay" onClick={handleClosePopup}>
+                <div className="popup-box2" onClick={e => e.stopPropagation()} style={{ paddingLeft: '30px', paddingRight: '30px', paddingTop: '30px' }}>
+                  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '30px' }}>
+                    <h3>Pridať fotky</h3>
+                    <span style={{ cursor: 'pointer', fontSize: '30px', }} onClick={handleClosePopup}>x</span>
+                  </div>
+                  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingBottom: '60px', border: 'dashed', borderWidth: '1px', borderColor: '#DDDDDD'}}>
+                    <AddPhotoAlternateIcon color="black" fontSize="large" style={{paddingBottom: '20px', paddingTop: '20px'}}/>
+                    <div className="addBoxText" style={{ textAlign: 'center', paddingBottom: '20px' }}>Sem presuňte fotky</div>
+                    <div className="addBoxMiddleText" style={{ textAlign: 'center', paddingBottom: '20px'  }}>alebo</div>
+                    <div className="addBoxText" style={{width: '130px', paddingTop: '5px', paddingBottom: '5px', textAlign: 'center', border: 'solid', borderWidth: '2px', borderRadius: '4px' }}>Vyberte súbory</div>
+                  </div>
+                  <div style={{ paddingTop: '40px' }}>
+                    <button type="submit" style={{ height: '60px' }}>Pridať</button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </>
+        )}
 
       {/* Simple image preview */}
       {previewImage && (
